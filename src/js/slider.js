540bcf3a04f80ed40dadc7db27ae68b53bd64d4b
Evelyn Paola Lucca <eve-lanegra@hotmail.com>
2022-06-05 09:18:52 -0300

    primer commit

---
+++ src/js/slider.js
@@ -0,0 +1,41 @@
+const leftArrows = document.querySelectorAll('.left-arrow');
+const rightArrows = document.querySelectorAll('.right-arrow');
+
+console.log(leftArrows);
+console.log(rightArrows);
+
+let contadorRight = 0;
+
+
+rightArrows.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+        const carrousel = el.previousElementSibling;
+
+        if(contadorRight < (carrousel.childElementCount - 1)){
+            contadorRight++
+        } else{
+            contadorRight = 0;
+        }
+
+        carrousel.style.left = `-${contadorRight * 100}%`;
+    })
+
+})
+
+leftArrows.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+        const carrousel = el.nextElementSibling;
+
+        console.log(contadorRight);
+        if(contadorRight > 0){
+            contadorRight--
+        } else{
+            contadorRight = parseInt(carrousel.childElementCount - 1);
+        }
+
+        carrousel.style.left = `-${contadorRight * 100}%`;
+    })
+
+})
