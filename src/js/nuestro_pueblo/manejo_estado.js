3c2c0b8f12d3fdf20985646e4c3a94dd59fc0d3c
EVELYNLUCCA <eve-lanegra@hotmail.com>
2022-07-22 19:08:10 -0300

    animation nuestro pueblo

--- src/js/nuestro_pueblo/manejo_estado.js
+++ src/js/nuestro_pueblo/manejo_estado.js
@@ -1,34 +1,43 @@
 import data from '../../nuestro_pueblo.json' assert { type: "json" };
 const card = document.getElementById('card');
 const wrapper = document.getElementById('wrapper');
+const card_container = document.getElementById('card_container');
+const imagesSlider = document.querySelector('.images_slider');
+console.log(card_container);
 
 const activadores = document.querySelectorAll('.select_picture');
-console.log(activadores);
 
 activadores.forEach(activador => {
     activador.addEventListener('click', (e) => {
-        const filtro = data.filter(el => el.id === e.target.id)[0];
+        card_container.style.animation = 'desappear 1.2s ease-out';
+
+        setTimeout(() => {
+            const filtro = data.filter(el => el.id === e.target.id)[0];
         
-        card.innerHTML = `
-            <img src="${filtro.imagenPrincipal}" alt="${filtro.id}">
-            <div class="text">
-                <h2>${filtro.titulo}</h2>
-                <p>${filtro.descripción}</p>
-            </div>
-            <div class="border" style="background: ${filtro.color}"></div>
-        `;
-
-        wrapper.innerHTML = '';
-
-        filtro.imagenesMuestra.forEach((el, index) => {
-            const elementHtml = `
-                <input type="radio" name="image-slide" checked="checked">
-                <picture>
-                    <img src="${el}" alt="${filtro.id + ' image ' + index}">
-                </picture>
+            card.innerHTML = `
+                <img src="${filtro.imagenPrincipal}" alt="${filtro.id}">
+                <div class="text">
+                    <h2>${filtro.titulo}</h2>
+                    <p>${filtro.descripción}</p>
+                </div>
+                <div class="border" style="background: ${filtro.color}"></div>
             `;
+    
+            wrapper.innerHTML = '';
+    
+            filtro.imagenesMuestra.forEach((el, index) => {
+                const elementHtml = `
+                    <input type="radio" name="image-slide" checked="checked">
+                    <picture>
+                        <img src="${el}" alt="${filtro.id + ' image ' + index}">
+                    </picture>
+                `;
+    
+                wrapper.innerHTML += elementHtml;
+            });   
+            
+            card_container.style.animation = 'appear 1.2s ease-out';
+        }, 1000);
 
-            wrapper.innerHTML += elementHtml;
-        });
     })
 })
