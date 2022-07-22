6d2832075f0d4d83fa34890f4fd1fab84afec58e
EVELYNLUCCA <eve-lanegra@hotmail.com>
2022-07-22 12:38:37 -0300

    modificando nuestro pueblo

--- src/js/nuestro_pueblo/manejo_estado.js
+++ src/js/nuestro_pueblo/manejo_estado.js
@@ -1,39 +1,4 @@
-const data = {
-    club: {
-        imgs: [
-            {
-                url: './src/images/club Sportivo Roca/club.jpg',
-                description: 'FOTO DEL FRENTE DEL CLUB'
-            },
-            {
-                url: './src/images/club Sportivo Roca/campeonas1980.jpg',
-                description: 'EQUIPO CAMPEÓN 1980'
-            },
-            {
-                url: './src/images/club Sportivo Roca/cancha.jpg',
-                description: 'CANCHA'
-            }
-        ],
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt perspiciatis est beatae dolorem, temporibus earum asperiores maxime iusto molestiae numquam quia molestias aliquam tempore error laboriosam? Dignissimos libero odit ut iusto, eligendi rem adipisci vero perspiciatis, officiis a ratione aperiam, voluptatem accusamus nobis. Similique sapiente,'
-    },
-    plaza: {
-        imgs: [
-            {
-                url: './src/images/plaza/plaza_frente.jpg',
-                description: 'FRENTE PLAZA'
-            },
-            {
-                url: './src/images/plaza_canteros/arreglo.jpg',
-                description: 'CANTEROS PLAZA'
-            },
-            {
-                url: './src/images/plaza_juegos/plaza_juegos.jpg',
-                description: 'JUEGOS PLAZA'
-            }
-        ],
-        description: 'Lorem <img src="./src/images/plaza/plaza_frente.jpg" alt="frente de plaza"/> ipsum dolor sit amet consectetur adipisicing elit. Nesciunt perspiciatis est beatae dolorem, temporibus earum asperiores maxime iusto molestiae numquam quia molestias aliquam tempore error laboriosam? Dignissimos libero odit ut iusto, eligendi rem adipisci vero perspiciatis, officiis a ratione aperiam, voluptatem accusamus nobis. Similique sapiente,22222222'
-    }
-};
+
 
 const activadores = document.querySelectorAll('.select_picture');
 const slider_container = document.querySelector('.slider_presentacion');
